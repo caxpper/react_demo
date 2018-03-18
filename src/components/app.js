941fabec16dba05841f70b8b943cbbccbbffc70d
Alejandro Gaspar <caxpper@hotmail.com>
2018-03-17 22:45:47 -0700

    final functional components demo

--- src/components/app.js
+++ src/components/app.js
@@ -4,18 +4,27 @@ import logo from '../assets/images/logo.svg';
 import shamrockL from '../assets/images/shamrock-l.png';
 import shamrockR from '../assets/images/shamrock-r.png';
 import gold from '../assets/images/pot-o-gold.png';
+import Greeting from './greeting';
+
+const user = {
+    name: 'Fredo',
+    favColor: 'blue',
+    doStuff: function(){
+        
+    }
+};
 
 const App = () => (
     <div>
-        <div className="app">
+        <div className="app">              
             <img src={gold} className="gold left" />
             <img src={logo} className="logo rotate"/>
             <div className="title-container">
                 <img src={shamrockL} className="clover left rotate3d-right" />
-                <h1>Welcome to React</h1>
+                <Greeting {...user} />
                 <img src={shamrockR} className="clover right rotate3d-left" />
             </div>
-            <img src={gold} className="gold right"/>
+            <img src={gold} className="gold right"/>            
         </div>
     </div>
 );
